f6884a942147a760d6d1c76c90b2ab6aa8db7edf
chico <chico.zimcolor@gmail.com>
2025-11-08 12:54:38 -0300

    option service updated

--- src/helper/options-service.js
+++ src/helper/options-service.js
@@ -2,18 +2,18 @@ import Config from '../commands/config/config.js';
 
 export default class OptionsService {
   static update({ options, default: defaultOptions, specificConfig }) {
-    const mergedOptions = { ...defaultOptions, ...options };
     let config = Config.get();
     if (specificConfig) {
       const specifics = specificConfig.get();
       config = { ...config, ...specifics };
     }
 
-    for (const [key, value] of Object.entries(config)) {
-      if (value !== undefined && options[key] === undefined) {
-        mergedOptions[key] = value;
+    const filteredOptions = {};
+    for (const [key, value] of Object.entries(options)) {
+      if (value !== undefined) {
+        filteredOptions[key] = value;
       }
     }
-    return mergedOptions;
+    return { ...defaultOptions, ...config, ...filteredOptions };
   }
 }
